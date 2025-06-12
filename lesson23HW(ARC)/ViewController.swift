a43cf017909c956f54c2e683098bc7a92ee44b8a
PetrushenkoDmitry <petrushenkodmitrii@yandex.ru>
2025-06-12 17:21:02 +0300

    Initial Commit

---
+++ lesson23HW(ARC)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson23HW(ARC)
+//
+//  Created by Дмитрий Петрушенко on 12/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
